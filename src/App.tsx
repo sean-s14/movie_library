80f1a40f7b6fa9ffe4fb57f52bff3faac12a7363
Sean Stocker <sean.stocker14@gmail.com>
2023-01-29 20:08:05 +0000

    Initialized project with vite. Made all necessary configurations, absolute imports, more optimal chunking, ssl certificates for development. Installed necessary packages, mui and vite plugin to remove console logs for production.

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// App.tsx
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h2>Welcome to</h2>
+        <h1>Sean's Movie Library</h1>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
