0b8b6378594c32417de0c10b822de8d9c0e08f54
Sean Stocker <sean.stocker14@gmail.com>
2023-02-07 22:34:29 +0000

    Stylistic changes. Background (gradient + tmdb logo), glassmorphism effect on card component, smooth auto-scroll on page change.

--- src/routeContainer.tsx
+++ src/routeContainer.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import tmdbLogo from "src/assets/tmdb_logo.svg";
 
 export default function RouteContainer(props: any) {
   return (
@@ -9,9 +10,26 @@ export default function RouteContainer(props: any) {
         justifyContent: "center",
         py: 10,
         minHeight: "calc(100vh - 70px)",
+        /* fallback for old browsers */
+        // background: "#0f2027",
+        /* Chrome 10-25, Safari 5.1-6 */
+        // background:
+        //   "-webkit-linear-gradient(to right, #0f2027, #203a43, #2c5364)",
+        // backgroundImage: "linear-gradient(45deg, #141e30, #243b55)",
+        backgroundImage: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
         ...props.sx,
       }}
     >
+      <img
+        src={tmdbLogo}
+        alt="TMDB Logo"
+        style={{
+          position: "fixed",
+          top: 450,
+          margin: 30,
+          zIndex: 0,
+        }}
+      />
       {props.children}
     </Box>
   );
--- src/routes/movieList.tsx
+++ src/routes/movieList.tsx
@@ -1,3 +1,4 @@
+import "./movieList.css";
 import { useState, useEffect } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAxios } from "src/hooks/exports";
@@ -51,6 +52,11 @@ function App() {
     setPageNum(1); // This resets the page number whenever the user changes genre
     refetchMovideData();
   }, [genre]);
+
+  useEffect(() => {
+    // window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" }); // Does'nt work on safari as of July 2021
+  }, [pageNum]);
 
   /**
    * Retrieves array of genres from queryClient with hash ["movie", "genres"].
@@ -133,12 +139,16 @@ function App() {
       {/* Movie List */}
       <Grid container spacing={2} rowSpacing={6} disableEqualOverflow>
         {movideDataIsFetching
-          ? [1, 2, 3, 4, 5, 6, 7, 8].map((num, index) => (
+          ? [...new Uint8Array(20)].map((num, index) => (
               <Grid
                 key={index}
-                xs={6}
-                sm={4}
+                xs={12}
+                xs475={6}
+                sm={6}
+                sm650={4}
+                sm750={4}
                 md={3}
+                md1050={2.4}
                 xl={2}
                 display="flex"
                 justifyContent="center"
@@ -155,7 +165,7 @@ function App() {
                       animation="wave"
                       variant="rectangular"
                     />
-                    <CardContent sx={{ height: 160 }}>
+                    <CardContent sx={{ height: 160 }} className="glassmorphism">
                       <Skeleton animation="wave" height={20} width="30%" />
                       <Skeleton animation="wave" height={30} width="90%" />
                       <Skeleton animation="wave" height={15} width="70%" />
@@ -179,10 +189,19 @@ function App() {
                 justifyContent="center"
                 alignItems="center"
               >
-                <Paper elevation={12}>
+                <Paper
+                  elevation={12}
+                  sx={{
+                    borderRadius: 3,
+                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                    zIndex: 1,
+                  }}
+                >
                   <Card
                     sx={{
                       width: 185,
+                      borderRadius: "inherit",
+                      backgroundColor: "rgba(0, 0, 0, 0)",
                     }}
                   >
                     <CardMedia
@@ -191,7 +210,13 @@ function App() {
                       alt={movie.title}
                       height={277}
                     />
-                    <CardContent sx={{ height: 130 }}>
+                    <CardContent
+                      sx={{
+                        height: 130,
+                        color: grey[100],
+                      }}
+                      className="glassmorphism"
+                    >
                       <Avatar
                         sx={{
                           bgcolor: "black",
@@ -216,12 +241,16 @@ function App() {
                           (movie?.vote_average && movie.vote_average * 10) || 0
                         }
                       />
-                      <Typography variant={"body1"} sx={{ mt: 1 }} gutterBottom>
+                      <Typography
+                        variant={"body1"}
+                        sx={{ mt: 1, color: "inherit", fontWeight: 700 }}
+                        gutterBottom
+                      >
                         {movie.title}
                       </Typography>
                       <Typography
                         variant={"subtitle2"}
-                        sx={{ color: grey[500] }}
+                        sx={{ color: grey[300] }}
                       >
                         {movie.release_date &&
                           dateConverter(movie.release_date, true)}
